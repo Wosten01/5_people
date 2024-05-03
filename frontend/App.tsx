e1bc39006d7fb49888c126ec8cff69227d367e10
Vladislav <mribloko@gmail.com>
2024-05-03 15:17:52 +0400

    Added navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,23 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, Button, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./pages/Home";
 
-export default function App() {
+if (process.env.NODE_ENV === "development" && Platform.OS === "web") {
+  require("@expo/metro-runtime"); // #23104 (comment)
+}
+
+const Stack = createNativeStackNavigator();
+
+interface DeatailScreenProps {
+  navigation: any;
+}
+
+function DetailsScreen({ navigation }: DeatailScreenProps) {
   return (
-    <View style={styles.container}>
-      <Text>Fuck you!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ title: "Main" }}>
+          {(props) => <HomeScreen {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  aboba: {
-    color: "red"
-  }
-});
+export default App;
