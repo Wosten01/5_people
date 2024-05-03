2d27507cf52e8dbb3cfcf1ad2072cdbc811c1fb9
Vladislav <mribloko@gmail.com>
2024-05-03 18:42:24 +0400

    Added main menu without map

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,45 +1,21 @@
 import * as React from "react";
-import { View, Text, Button, Platform, StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "./pages/Home";
 
-import {
-  Appbar,
-  Card,
-  DefaultTheme,
-  FAB,
-  MD3LightTheme,
-  PaperProvider,
-} from "react-native-paper";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
+import { HomeScreen } from "./src/pages/Home";
+import { DetailsScreen } from "./src/pages/Detail";
 
 if (process.env.NODE_ENV === "development" && Platform.OS === "web") {
   require("@expo/metro-runtime");
 }
-
 const theme = {
   ...DefaultTheme,
 };
 
 const Stack = createNativeStackNavigator();
 
-interface DeatailScreenProps {
-  navigation: any;
-}
-
-function DetailsScreen({ navigation }: DeatailScreenProps) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push("Details")}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
 function App() {
   return (
     <PaperProvider theme={theme}>
@@ -56,9 +32,3 @@ function App() {
 }
 
 export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-});
